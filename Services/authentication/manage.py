f86e67421d2eafe37699420d72c283cab45bb05e
unknown <jokas3@gmail.com>
2025-04-22 21:30:48 +0200

    feat: Change server port

--- Services/authentication/manage.py
+++ Services/authentication/manage.py
@@ -2,13 +2,25 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
+        load_dotenv()
+
+        host = os.getenv('DJANGO_HOST')
+        port = os.getenv('DJANGO_PORT')
+
         from django.core.management import execute_from_command_line
+
+        # Inject host and port into the command if none given
+
+        if len(sys.argv) == 1 or (sys.argv[1] == 'runserver' and len(sys.argv) == 2):
+            sys.argv += [f'{host}:{port}']
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
