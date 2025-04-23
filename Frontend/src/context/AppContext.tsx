bdb450c2a2548c5d6752cc981332d73f9791444a
unknown <jokas3@gmail.com>
2025-04-23 18:03:44 +0200

    fix: Fixed errors in AppContext.jsx

--- Frontend/src/context/AppContext.tsx
+++ Frontend/src/context/AppContext.tsx
@@ -4,10 +4,14 @@ import AppContextType from "@/types/AppContextType.tsx";
 const AppContext = createContext<AppContextType>({
     gateway: {
         authentication: "",
-        exercise: ""
+        exercise: "",
     },
+
+    // User Variables
     user: {},
     setUser: () => {},
+    authenticated: false,
+    setAuthenticated: () => {},
 });
 
 export default AppContext;
