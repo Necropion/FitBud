3d71b81edf3d9ebf3116b1c8222260398a452863
unknown <jokas3@gmail.com>
2025-04-30 21:28:04 +0200

    feat: Dockerize Gateway

--- Services/gateway/main.py
+++ Services/gateway/main.py
@@ -1,20 +1,38 @@
 from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
 import httpx
 
 app = FastAPI()
 
+# Decide Environment
+env = os.getenv("ENV", "Development")
+
+if env == "Production":
+    load_dotenv(".env.Production")
+
+if env == "Development":
+    load_dotenv(".env.Development")
+
+frontend_url = os.getenv("FRONTEND_URL")
+allow_origins = ["http://localhost:5173"]
+if frontend_url:
+    allow_origins.append(frontend_url)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+
 services = {
-    "authentication": "http://localhost:8010",
-    "training": "http://localhost:8020",
+    "authentication": os.environ["AUTHENTICATION_SERVICE_URL"],
+    "training": os.environ["TRAINING_SERVICE_URL"],
 }
 
 async def forward_request(service_url: str, method: str, path: str, body=None, headers=None):
